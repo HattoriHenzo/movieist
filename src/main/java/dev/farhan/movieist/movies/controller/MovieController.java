4ca51628466f494fe2bca21c62fb04871fa154f0
HattoriHenzo <esombugma@gmail.com>
2025-01-30 20:11:39 -0500

    Minor refactoring in the code

--- src/main/java/dev/farhan/movieist/movies/controller/MovieController.java
+++ src/main/java/dev/farhan/movieist/movies/controller/MovieController.java
@@ -24,11 +24,11 @@ public class MovieController {
 
     @GetMapping
     public ResponseEntity<List<Movie>> getMovies() {
-        return new ResponseEntity<List<Movie>>(service.findAllMovies(), HttpStatus.OK);
+        return new ResponseEntity<>(service.findAllMovies(), HttpStatus.OK);
     }
 
     @GetMapping("/{imdbId}")
     public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable String imdbId){
-        return new ResponseEntity<Optional<Movie>>(service.findMovieByImdbId(imdbId), HttpStatus.OK);
+        return new ResponseEntity<>(service.findMovieByImdbId(imdbId), HttpStatus.OK);
     }
 }
